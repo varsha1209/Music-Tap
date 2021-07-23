dc46c2423d11c848873926fa5e3ba209eb420947
Varsha <varshadahare1209@gmail.com>
2021-07-24 00:57:07 +0530

    -v1 commit

---
+++ src/Components/Player.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "./Footer";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+
+function Player({ spotify }) {
+  return (
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body spotify={spotify} />
+      </div>
+      <Footer spotify={spotify} />
+    </div>
+  );
+}
+
+export default Player;
